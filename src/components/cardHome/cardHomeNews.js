a2a12ab21c31d5d4e5d375a2eba9be03d9b18b2c
Pablo Cortes <47508769+pablitto25@users.noreply.github.com>
2024-04-19 14:51:21 -0300

    changed news description

--- src/components/cardHome/cardHomeNews.js
+++ src/components/cardHome/cardHomeNews.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './cardHomeNews.css'; 
 
+/* 
+Jackery tiene como principal propósito brindar energía solar para todas las personas que lo precisen, sea que la requieran para actividades outdoor, indoor, o, bien, para casos de emergencia, salud, trabajo, entre otros. En efecto, Jackery se presenta como aquella solución multifuncional que se adapta a cualquier tipo de circunstancia.
+ */
+
 
 const CardHomeNew = () => {
   return (
@@ -8,8 +12,8 @@ const CardHomeNew = () => {
       <div className='conteinerBodyText'>
         {/* <p className='homeText'>Nuevos ingresos 2024</p> */}
         <h1 className='homeTextTitle'>Jackery, la solución multifuncional</h1>
-        <span className='homeText'>Descubre todo lo que puedes hacer con los</span>
-        <span className='homeText'>productos de Jackery. Animate a inventar tu propio estilo de vida.</span>
+        <span className='homeText'>Jackery tiene como principal propósito brindar energía solar para todas las personas que lo precisen, sea que la requieran para actividades outdoor, indoor, o, bien, para casos de emergencia, salud, trabajo, entre otros. En efecto, Jackery se presenta como aquella solución multifuncional que se adapta a cualquier tipo de circunstancia.</span>
+        
       </div>
       <a href="/PRODUCTOS/Jackery%20Generador%20Solar%201000%20-%202%20paneles" className="myButtonHome featuredProductButton" rel="noopener noreferrer"> 
         EXPLORAR MÁS
